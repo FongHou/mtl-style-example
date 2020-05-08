a8605453e50a5eb3b3552a10b4ef1f2118d7de4f
Fong Hou <haskeller@win-x1-laptop.localdomain>
2020-05-08 16:37:06 -0400

    cleanup

--- library/MTLStyleExample/Main.hs
+++ library/MTLStyleExample/Main.hs
@@ -1,20 +1,21 @@
-module MTLStyleExample.Main ( main ) where
+module MTLStyleExample.Main (main) where
 
-import Control.Monad.Logger ( MonadLogger(..), logInfoN )
-import Control.Monad.Time ( MonadTime(..) )
-
-import Data.Semigroup ( (<>) )
+import Control.Monad.Logger (MonadLogger (..), logInfoN)
+import Control.Monad.Time (MonadTime (..))
 import qualified Data.Text as T
-import Data.Time.Clock ( diffUTCTime )
-
+import Data.Time.Clock (diffUTCTime)
 import MTLStyleExample.Interfaces
-
-import Prelude hiding ( readFile )
+import Prelude hiding (readFile)
 
 --------------------------------------------------------------------------------
 -- Logic
-main
-  :: (MonadArguments m, MonadFileSystem m, MonadLogger m, MonadTime m) => m ()
+main ::
+  ( MonadArguments m,
+    MonadFileSystem m,
+    MonadLogger m,
+    MonadTime m
+  ) =>
+  m ()
 main = do
   startTime <- currentTime
   fileName <- getArgs
@@ -22,5 +23,5 @@ main = do
   logInfoN $ "Hello, " <> target <> "!"
   endTime <- currentTime
   let duration = endTime `diffUTCTime` startTime
-  logInfoN
-    $ T.pack (show (round (duration * 1000) :: Integer)) <> " milliseconds"
+  logInfoN $
+    T.pack (show (round (duration * 1000) :: Integer)) <> " milliseconds"
--- test-suite/Bench.hs
+++ test-suite/Bench.hs
@@ -1,91 +1,72 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Main ( main ) where
+module Main (main) where
 
 import Control.Monad
 import Control.Monad.Freer as Freer
 import Control.Monad.Freer.Error as Freer
 import Control.Monad.Freer.Input as Freer
 import Control.Monad.Freer.Output as Freer
-import Control.Monad.IO.Class
-import Control.Monad.Logger
-import Control.Monad.Time ( MonadTime(..) )
-
-import Data.ByteString ( ByteString )
-import Data.Function ( (&) )
-import Data.Functor.Identity ( runIdentity )
-import Data.Text ( Text )
-import Data.Time.Clock.POSIX ( posixSecondsToUTCTime )
-
+import Data.ByteString (ByteString)
+import Data.Function ((&))
+import Data.Functor.Identity (runIdentity)
+import Data.Text (Text)
+import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import FreerExample.Test.Stubs as Freer
-
 import Gauge
-import Gauge.Main ( defaultMain )
-
-import MTLStyleExample.Interfaces as MTL
 import qualified MTLStyleExample.Main
 import MTLStyleExample.Test.Stubs as MTL
-
-import MTLStyleExample2.Test.Stubs as CPS
-
+import MTLStyleExample2.Test.Stubs as MTL2
 import qualified Polysemy as Poly
 import qualified Polysemy.Error as Poly
 import qualified Polysemy.Input as Poly
 import qualified Polysemy.Output as Poly
-
 import qualified PolysemyExample.Test.Stubs as Poly
 
---------------------------------------------------------------------------------
--- IO wiring
-newtype AppM a = AppM (LoggingT IO a)
-  deriving ( Functor, Applicative, Monad, MonadIO, MonadLogger, MonadFileSystem
-           , MonadArguments, MonadTime )
-
-runAppM :: AppM a -> IO a
-runAppM (AppM x) = runStderrLoggingT x
-
 mtl :: ((), [ByteString])
-mtl = MTLStyleExample.Main.main
-  & MTL.runArgumentsT ["sample.txt"]
-  & MTL.runFileSystemT [("sample.txt", "Alyssa")]
-  & MTL.runLoggerT
-  & MTL.runTickingClockT (posixSecondsToUTCTime 0)
-  & runIdentity
-
-cps :: ((), [ByteString])
-cps = CPS.runTest
+mtl =
   MTLStyleExample.Main.main
-  ["sample.txt"]
-  (FileSystem [("sample.txt", "World")])
-  (posixSecondsToUTCTime 0)
+    & MTL.runArgumentsT ["sample.txt"]
+    & MTL.runFileSystemT [("sample.txt", "Alyssa")]
+    & MTL.runLoggerT
+    & MTL.runTickingClockT (posixSecondsToUTCTime 0)
+    & runIdentity
 
-freer :: _
-freer = MTLStyleExample.Main.main
-  & Freer.runArguments ["sample.txt" :: Text]
-  & Freer.runFileSystem
-  & Freer.runInputConst (Freer.FS [("sample.txt", "World")])
-  & Freer.runTickingClock (posixSecondsToUTCTime 0) 1
-  -- & Freer.runError @String
-  & Freer.runLogger
-  & Freer.runOutputList @Text
-  & Freer.runError @String
-  & Freer.run
+mtl2 :: ((), [ByteString])
+mtl2 =
+  MTL2.runTest
+    MTLStyleExample.Main.main
+    ["sample.txt"]
+    (FileSystem [("sample.txt", "World")])
+    (posixSecondsToUTCTime 0)
 
-polysemy :: _
-polysemy = MTLStyleExample.Main.main
-  & Poly.runArguments
-  & Poly.runInputConst ["sample.txt" :: Text]
-  & Poly.runFileSystem
-  & Poly.runInputConst (Poly.FS [("sample.txt", "World")])
-  & Poly.runTickingClock (posixSecondsToUTCTime 0) 1
-  -- & Poly.runError @String
-  & Poly.runLogger
-  & Poly.runOutputList @ByteString
-  & Poly.runError @String
-  & Poly.run
+freer :: (Either String ([Text], ()))
+freer =
+  MTLStyleExample.Main.main
+    & Freer.runArguments ["sample.txt" :: Text]
+    & Freer.runFileSystem
+    & Freer.runInputConst (Freer.FS [("sample.txt", "World")])
+    & Freer.runTickingClock (posixSecondsToUTCTime 0) 1
+    & Freer.runLogger
+    & Freer.runOutputList @Text
+    & Freer.runError @String
+    & Freer.run
 
-once :: Monad m => m a -> Int -> m _
+polysemy :: (Either String ([ByteString], ()))
+polysemy =
+  MTLStyleExample.Main.main
+    & Poly.runArguments
+    & Poly.runInputConst ["sample.txt" :: Text]
+    & Poly.runFileSystem
+    & Poly.runInputConst (Poly.FS [("sample.txt", "World")])
+    & Poly.runTickingClock (posixSecondsToUTCTime 0) 1
+    & Poly.runLogger
+    & Poly.runOutputList @ByteString
+    & Poly.runError @String
+    & Poly.run
+
+once :: Monad m => m a -> Int -> m a
 once m _ = m
 
 runs :: Monad m => m a -> Int -> m Int
@@ -97,14 +78,17 @@ n :: Int
 n = 1000
 
 main :: IO ()
-main = defaultMain
-  [ bgroup
-      "effect-style-bench"
-      [ bench "mtl" $ nf (once mtl) n
-      , bench "freer" $ nf (once freer) n
-      , bench "polysemy" $ nf (once polysemy) n
-      , bench "cps" $ nf (once cps) n
-      , bench "mtl/1000" $ nf (runs mtl) n
-      , bench "freer/1000" $ nf (runs freer) n
-      , bench "polysemy/1000" $ nf (runs polysemy) n
-      , bench "cps/1000" $ nf (runs cps) n]]
+main =
+  defaultMain
+    [ bgroup
+        "effect-style-bench"
+        [ bench "mtl" $ nf (once mtl) n,
+          bench "mtl2" $ nf (once mtl) n,
+          bench "freer" $ nf (once freer) n,
+          bench "polysemy" $ nf (once polysemy) n,
+          bench "mtl/1000" $ nf (runs mtl) n,
+          bench "mtl2/1000" $ nf (runs mtl2) n,
+          bench "freer/1000" $ nf (runs freer) n,
+          bench "polysemy/1000" $ nf (runs polysemy) n
+        ]
+    ]
